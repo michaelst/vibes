296457fd9550c1753bf60d4d0d37531dee5ea6b2
Michael St Clair <michaelst57@gmail.com>
2024-02-10 17:47:37 -0700

    setup builds

---
+++ .devops/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+cd "`dirname $0`"/..
+
+COMMIT_SHA=$(git rev-parse HEAD)
+docker buildx build -t ghcr.io/vibes/vibes:$COMMIT_SHA --push .
+
+helm upgrade --install vibes oci://ghcr.io/michaelst/helm/cloud-57 \
+  -f .devops/values.yaml \
+  --set image.repository=ghcr.io/vibes/vibes \
+  --set image.tag=$COMMIT_SHA \
+  --version 1.0.3 \
+  --atomic \
+  -n vibes
